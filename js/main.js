a138781692116b0b719036fe90983b38dfa721a4
godinuis123 <godinus12@gmail.com>
2019-11-03 22:04:08 +0900

    NeoWine.Co

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* =================================
 ------------------------------------
-	Template Name: Industry.INC 
-	Description: Industry.INC HTML Template
+	Template Name: NeoWine.Co 
+	Description: NeoWine.Co HTML Template
 	Author: colorlib
 	Author URI: https://www.colorlib.com/
 	Version: 1.0
@@ -13,166 +13,165 @@
 'use strict';
 
 $(window).on('load', function() {
-	/*------------------
-		Preloder
-	--------------------*/
-	$(".loader").fadeOut();
-	$("#preloder").delay(400).fadeOut("slow");
+    /*------------------
+    	Preloder
+    --------------------*/
+    $(".loader").fadeOut();
+    $("#preloder").delay(400).fadeOut("slow");
 
 });
 
 (function($) {
-	/*------------------
-		Navigation
-	--------------------*/
-	$('.site-nav-menu > ul').slicknav({
-		appendTo:'.header-section',
-		closedSymbol: '<i class="fa fa-angle-down"></i>',
-		openedSymbol: '<i class="fa fa-angle-up"></i>',
-		allowParentLinks: true
-	});
+    /*------------------
+    	Navigation
+    --------------------*/
+    $('.site-nav-menu > ul').slicknav({
+        appendTo: '.header-section',
+        closedSymbol: '<i class="fa fa-angle-down"></i>',
+        openedSymbol: '<i class="fa fa-angle-up"></i>',
+        allowParentLinks: true
+    });
 
-	$('.slicknav_nav').append('<li class="search-switch-warp"><button class="search-switch"><i class="fa fa-search"></i></button></li>');
-
-
-	/*------------------
-		Search model
-	--------------------*/
-	$('.search-switch').on('click', function() {
-		$('.search-model').fadeIn(400);
-	});
-
-	$('.search-close-switch').on('click', function() {
-		$('.search-model').fadeOut(400,function(){
-			$('#search-input').val('');
-		});
-	});
+    $('.slicknav_nav').append('<li class="search-switch-warp"><button class="search-switch"><i class="fa fa-search"></i></button></li>');
 
 
-	/*------------------
-		Background Set
-	--------------------*/
-	$('.set-bg').each(function() {
-		var bg = $(this).data('setbg');
-		$(this).css('background-image', 'url(' + bg + ')');
-	});
+    /*------------------
+    	Search model
+    --------------------*/
+    $('.search-switch').on('click', function() {
+        $('.search-model').fadeIn(400);
+    });
+
+    $('.search-close-switch').on('click', function() {
+        $('.search-model').fadeOut(400, function() {
+            $('#search-input').val('');
+        });
+    });
 
 
-	/*------------------
-		Hero Slider
-	--------------------*/
-	$('.hero-slider').owlCarousel({
-		nav: true,
-		dots: false,
-		loop: true,
-		navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		autoplay: true,
-		items: 1,
-		animateOut: 'fadeOut',
-    	animateIn: 'fadeIn',
-	});
+    /*------------------
+    	Background Set
+    --------------------*/
+    $('.set-bg').each(function() {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
 
-	/*------------------
-		Brands Slider
-	--------------------*/
-	$('#client-carousel').owlCarousel({
-		nav: false,
-		loop: true,
-		margin:20,
-		autoplay: true,
-		responsive:{
-			0:{
-				items:2,
-				margin: 0
-			},
-			600:{
-				items:3
-			},
-			800:{
-				items:4
-			},
-			992:{
-				items:4
-			},
-			1200:{
-				items:5
-			},
-		}
-	});
 
-	/*---------------------
-		Testimonial Slider
-	----------------------*/
-	$('.testimonial-slider').owlCarousel({
-		nav: false,
-		dots: true,
-		loop: true,
-		autoplay: true,
-		items: 1,
-	});
+    /*------------------
+    	Hero Slider
+    --------------------*/
+    $('.hero-slider').owlCarousel({
+        nav: true,
+        dots: false,
+        loop: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        autoplay: true,
+        items: 1,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+    });
 
-	/*------------------
-		Image Popup
-	--------------------*/
-	$('.video-popup').magnificPopup({
-		type: 'iframe'
-	});
-	
-	/*------------------
-		Accordions
-	--------------------*/
-	$('.panel-link').on('click', function (e) {
-		$('.panel-link').parent('.panel-header').removeClass('active');
-		var $this = $(this).parent('.panel-header');
-		if (!$this.hasClass('active')) {
-			$this.addClass('active');
-		}
-		e.preventDefault();
-	});
+    /*------------------
+    	Brands Slider
+    --------------------*/
+    $('#client-carousel').owlCarousel({
+        nav: false,
+        loop: true,
+        margin: 20,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 2,
+                margin: 0
+            },
+            600: {
+                items: 3
+            },
+            800: {
+                items: 4
+            },
+            992: {
+                items: 4
+            },
+            1200: {
+                items: 5
+            },
+        }
+    });
 
-	/*------------------
-		Progress Bar
-	--------------------*/
-	$('.progress-bar-style').each(function() {
-		var progress = $(this).data("progress");
-		var prog_width = progress + '%';
-		if (progress <= 100) {
-			$(this).append('<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>');
-		}
-		else {
-			$(this).append('<div class="bar-inner" style="width:100%"><span>' + prog_width + '</span></div>');
-		}
-	});
+    /*---------------------
+    	Testimonial Slider
+    ----------------------*/
+    $('.testimonial-slider').owlCarousel({
+        nav: false,
+        dots: true,
+        loop: true,
+        autoplay: true,
+        items: 1,
+    });
 
-	/*------------------
-		Circle progress
-	--------------------*/
-	$('.circle-progress').each(function() {
-		var cpvalue = $(this).data("cpvalue");
-		var cpcolor = $(this).data("cpcolor");
-		var cpid 	= $(this).data("cpid");
+    /*------------------
+    	Image Popup
+    --------------------*/
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+    });
 
-		$(this).prepend('<div class="'+ cpid +' circle-warp"><h2>'+ cpvalue +'%</h2></div>');
+    /*------------------
+    	Accordions
+    --------------------*/
+    $('.panel-link').on('click', function(e) {
+        $('.panel-link').parent('.panel-header').removeClass('active');
+        var $this = $(this).parent('.panel-header');
+        if (!$this.hasClass('active')) {
+            $this.addClass('active');
+        }
+        e.preventDefault();
+    });
 
-		if (cpvalue < 100) {
+    /*------------------
+    	Progress Bar
+    --------------------*/
+    $('.progress-bar-style').each(function() {
+        var progress = $(this).data("progress");
+        var prog_width = progress + '%';
+        if (progress <= 100) {
+            $(this).append('<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>');
+        } else {
+            $(this).append('<div class="bar-inner" style="width:100%"><span>' + prog_width + '</span></div>');
+        }
+    });
 
-			$('.' + cpid).circleProgress({
-				value: '0.' + cpvalue,
-				size: 112,
-				thickness: 3,
-				fill: cpcolor,
-				emptyFill: "rgba(0, 0, 0, 0)"
-			});
-		} else {
-			$('.' + cpid).circleProgress({
-				value: 1,
-				size: 112,
-				thickness: 3,
-				fill: cpcolor,
-				emptyFill: "rgba(0, 0, 0, 0)"
-			});
-		}
+    /*------------------
+    	Circle progress
+    --------------------*/
+    $('.circle-progress').each(function() {
+        var cpvalue = $(this).data("cpvalue");
+        var cpcolor = $(this).data("cpcolor");
+        var cpid = $(this).data("cpid");
 
-	});
+        $(this).prepend('<div class="' + cpid + ' circle-warp"><h2>' + cpvalue + '%</h2></div>');
+
+        if (cpvalue < 100) {
+
+            $('.' + cpid).circleProgress({
+                value: '0.' + cpvalue,
+                size: 112,
+                thickness: 3,
+                fill: cpcolor,
+                emptyFill: "rgba(0, 0, 0, 0)"
+            });
+        } else {
+            $('.' + cpid).circleProgress({
+                value: 1,
+                size: 112,
+                thickness: 3,
+                fill: cpcolor,
+                emptyFill: "rgba(0, 0, 0, 0)"
+            });
+        }
+
+    });
 
 })(jQuery);
